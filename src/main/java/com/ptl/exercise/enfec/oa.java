07a39ee099786c26a57c3d1759cba74baf142454
achen-temp <chendongguo@gmail.com>
2023-10-04 15:30:22 -0400

    add enfec test problems

---
+++ src/main/java/com/ptl/exercise/enfec/oa.java
@@ -0,0 +1,28 @@
+package com.ptl.exercise.enfec;
+
+public class oa {
+
+    /**
+     * 1. given an integer array, find the 2nd largest number in this array
+     */
+
+
+    /**
+     * 2. given URL: https://jsonplaceholder.typicode.com/users
+     * You need to write a REST api to call the given URL and return a user with its "name, email, zipcode"
+     * for example, if user id is 1, you should return a JSON file with username "Bret", email "Sincere@april.biz" and zipcode "92998-3874"
+     * HINT: use "Rest Template"
+     *
+     * Result: OPEN a webbrowser and enter: http://localhost:8080/user/1  should return the correct result.
+     */
+
+    /**
+     * 3. Difference between thread Wait and Sleep
+     * 4. Difference between Enumeration and Iterator
+     * 5. Have you done migration to microservice, how do you do it and What is the challenge in migration to microservice
+     * 6. What is controller, service, repository and their orders in the application (order when a request comes)
+     * 7. What is lambda expression and given me one example about it
+     * 8. Tell me what do you know about authorization and authentication? (how do you do security)
+     * 9. What is the AWS cloud experience do you have?
+     */
+}
